647d3558bd7d16dc61943191216ed13514825a76
Shant Kasbarian <shantkasbarian06@gmail.com>
2025-03-24 16:15:53 +0400

    added conversion layer

---
+++ src/main/java/com/social_media/converters/PostConverter.java
@@ -0,0 +1,35 @@
+package com.social_media.converters;
+
+import com.social_media.entities.Post;
+import com.social_media.entities.User;
+import com.social_media.models.PostDto;
+import org.springframework.stereotype.Component;
+
+import javax.swing.text.DateFormatter;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
+@Component
+public class PostConverter implements Converter<Post, PostDto> {
+    @Override
+    public Post convertToEntity(PostDto model) {
+        Post post = new Post();
+        post.setId(model.id());
+        post.setTitle(model.title());
+        return post;
+    }
+
+    @Override
+    public PostDto convertToModel(Post entity) {
+        User user = entity.getUser();
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        return new PostDto(
+                entity.getId(),
+                user.getId(),
+                user.getUsername(),
+                entity.getTitle(),
+                entity.getLikes(),
+                entity.getPostedTime().format(dateTimeFormatter)
+        );
+    }
+}
