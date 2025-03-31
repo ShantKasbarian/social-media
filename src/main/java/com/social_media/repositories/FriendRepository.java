3d659a81d37957d7b680af3e6690478546962c0e
Shant Kasbarian <shantkasbarian06@gmail.com>
2025-03-31 16:47:15 +0400

    added tests for friendRepository'

--- src/main/java/com/social_media/repositories/FriendRepository.java
+++ src/main/java/com/social_media/repositories/FriendRepository.java
@@ -4,12 +4,10 @@ import com.social_media.entities.Friend;
 import com.social_media.entities.FriendshipStatus;
 import com.social_media.entities.User;
 import com.social_media.entities.UserFriend;
-import org.hibernate.annotations.processing.HQL;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
