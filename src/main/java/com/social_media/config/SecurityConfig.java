99967c99dcaad1398b45daa26cc812a8d10b7aab
Shant Kasbarian <shantkasbarian06@gmail.com>
2025-12-02 16:23:46 +0400

    externalize config in WebConfig

--- src/main/java/com/social_media/config/SecurityConfig.java
+++ src/main/java/com/social_media/config/SecurityConfig.java
@@ -1,7 +1,6 @@
 package com.social_media.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
